4ee62ce80ac646a9920425ff0b01c727f73b5310
Artyushenko Daniil <foxones80@gmail.com>
2025-06-27 15:11:30 +0600

    fixed some

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,7 +1,7 @@
 import { Swiper } from "swiper";
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation, Grid } from "swiper/modules";
 
-Swiper.use([Navigation, Pagination]);
+Swiper.use([Navigation, Pagination, Grid]);
 
 new Swiper(".work__slider", {
   slidesPerView: 4,
@@ -25,3 +25,34 @@ new Swiper(".work__slider", {
     },
   },
 });
+
+// new Swiper(".work__grid", {
+//   slidesPerView: 4,
+//   grid: {
+//     rows: 4,
+//     fill: "row",
+//   },
+//   spaceBetween: 20,
+//   navigation: {
+//     prevEl: ".work__arr--prev",
+//     nextEl: ".work__arr--next",
+//   },
+//   pagination: {
+//     el: ".work__pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 10,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     1024: {
+//       slidesPerView: 4,
+//       spaceBetween: 20,
+//     },
+//   },
+// });
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,71 @@
 import "./_components.js";
 import "./functions/burger.js";
+
+import { validateForms } from "./functions/validate-forms.js";
+
+function setupModalPopup() {
+  const modalBtns = document.querySelectorAll(".modal-btn");
+  const popup = document.querySelector(".popup--form");
+  if (!popup) return;
+  const closeBtns = popup.querySelectorAll(".popup__close");
+  const overlay = popup.querySelector(".popup__overlay");
+
+  modalBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      popup.classList.add("active");
+    });
+  });
+
+  function closePopup() {
+    popup.classList.remove("active");
+  }
+
+  closeBtns.forEach((btn) => {
+    btn.addEventListener("click", closePopup);
+  });
+  if (overlay) {
+    overlay.addEventListener("click", closePopup);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", setupModalPopup);
+
+const rules1 = [
+  {
+    ruleSelector: ".input-name",
+    rules: [
+      {
+        rule: "minLength",
+        value: 3,
+      },
+      {
+        rule: "required",
+        value: true,
+        errorMessage: "Заполните имя!",
+      },
+    ],
+  },
+  {
+    ruleSelector: ".input-tel",
+    tel: true,
+    telError: "Введите корректный телефон",
+    rules: [
+      {
+        rule: "required",
+        value: true,
+        errorMessage: "Заполните телефон!",
+      },
+    ],
+  },
+];
+
+const afterForm = () => {
+  const popupThx = document.querySelector(".popup--thx");
+  const popup = document.querySelector(".popup--form");
+
+  popup.classList.remove("active");
+  popupThx.classList.add("active");
+};
+
+validateForms(".popup__form", rules1, [], afterForm);
